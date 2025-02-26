d2202b832a6573ad583a7d3e4ad2842d49897cb3
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-02-26 09:51:05 -0800

    Update main.rs

--- examples/basic_network/src/main.rs
+++ examples/basic_network/src/main.rs
@@ -1,4 +1,19 @@
+use image::{DynamicImage, ImageFormat, ImageReader, Pixel, Rgb};
 use rcmlrs::*;
+
+fn scan() -> Result<(), std::io::Error> {
+    let img = ImageReader::open("path/to/image.png")?.decode()?;
+
+    let pixel: Rgb<u8> = img.get_pixel(10, 5).unwrap();
+
+    let red = pixel[0];
+    let green = pixel[1];
+    let blue = pixel[2];
+
+    println!("Pixel at (10, 5): R={}, G={}, B={}", red, green, blue);
+
+    Ok(())
+}
 
 fn main() {
     let weights: RamTensor = RamTensor::new_layer_zeros(Shape { x: 3, y: 3 }, 2); // TODO fix panic when this tensor is bigger
