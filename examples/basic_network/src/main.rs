588e87a7dd06f5c7239e823e0a561726b153ba20
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-02-27 22:16:27 -0800

    Update main.rs

--- examples/basic_network/src/main.rs
+++ examples/basic_network/src/main.rs
@@ -3,20 +3,24 @@ use rcmlrs::*;
 
 fn scan_image(image_name: &str) -> RamTensor {
     let path = format!("../Datasets/blackwhite/{}.png", image_name);
-    let img = ImageReader::open(path).unwrap().decode().unwrap();
+    let img = ImageReader::open(path)
+        .expect("No File at path!")
+        .decode()
+        .expect("Failed to decode image")
+        .into_rgb8();
 
-    let mut collected_pixels: Vec = Vec![];
+    let mut collected_pixels: Vec<Vec<Vec<f32>>> = vec![];
 
     for row in 0..50 {
-        collected_pixels.push(Vec![]);
+        collected_pixels.push(vec![]);
 
         for col in 0..50 {
-            let pixel: Rgb<u8> = img.get_pixel(row, col).unwrap();
+            let pixel = img.get_pixel(row, col); //Rgb<u8>
             let red = pixel[0];
             let green = pixel[1];
             let blue = pixel[2];
 
-            collected_pixels[row].push(Vec![red as f32, green as f32, blue as f32]);
+            collected_pixels[row as usize].push(vec![red as f32, green as f32, blue as f32]);
         }
     }
 
