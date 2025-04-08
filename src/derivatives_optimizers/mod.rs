f1057e363467d9593d5d156d99d0879cb0a5d89d
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2025-04-08 13:29:13 -0700

    Update mod.rs

--- src/derivatives_optimizers/mod.rs
+++ src/derivatives_optimizers/mod.rs
@@ -1,3 +1,5 @@
+use std::f32::EPSILON;
+
 use crate::*;
 
 // derivatives
@@ -18,10 +20,24 @@ impl RamTensor {
 
 //TODO
 // Hyperparameters:
-// - Learning rate
+// - Learning rate (0.001)
 // - First Moment Exponential Decay Rate
 // - Second Moment Exponential Decay Rate
-// - Epsilon
+// - Epsilon (f32::EPSILON)
 // - Weight Decay
 // - AMSGrad
-pub fn adam_optimizer(learning_rate: f32) {}
+pub fn custom_adam_optimizer(
+    alpha_learning_rate: f32,
+    beta_1: f32,
+    beta_2: f32,
+    delta_timestep: usize,
+) {
+}
+
+/// Recommanded learning rate 0.001
+pub fn adam_optimizer(learning_rate: f32, timestep: usize) {
+    let alpha: f32 = learning_rate;
+    let beta_1: f32 = 0.9;
+    let beta_2: f32 = 0.999;
+    let epsilon: f32 = EPSILON;
+}
